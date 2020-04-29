752eda9f11c6b5930517daba3508daa82cca5877
l0b5ter <44732631+l0b5ter@users.noreply.github.com>
2020-04-29 10:17:44 +0200

    Update Readme.mk

--- KeyDisplayed/Readme.mk
+++ KeyDisplayed/Readme.mk
@@ -1,18 +1,18 @@
-This is the module we are gonna use!! It detect keypress and show a webside regarding to that keycode. The letter e (101) is used to close the window, so dont use this as one of the keycodes.
-by jostein
+This module is made by MJØD, which they used on their project. It detect keypress and show a webside regarding to that keycode. The letter e (101) is used to close the window, so dont use this as one of the keycodes.
+
 
 
 Put this in config.js
 
 		{
 			module: "KeyDisplayed",
-			position: "top_center",
-			config: {
+			position: "top_center", //position (look here for all possible positions: https://forum.magicmirror.builders/topic/286/regions)
+			config: { // To overwrite defualt
 				url: [[113, "http://magicmirror.builders/"], [97, "https://vg.no"], [100, "https://www.dagbladet.no/"]], //[[KeyCode, "url"], [KeyCode, "url"]], Notice: 101 used to close window (key: e).
-				frameWidth: "700px",
-				frameHeight: "500px",
-				scrolling: "yes",
-				Displayed: 0
+				frameWidth: "700px", //popupwindow width
+				frameHeight: "500px", //popupwindow height
+				scrolling: "yes", //Sud you be able to scroll in the window?
+				Displayed: 0 //Start showing the popupwindow or hide (0 = hide, 1 = show)
 			}
 		},
 
